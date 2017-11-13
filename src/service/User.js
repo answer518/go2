827bb8011be77df49ee5deba0a61f3da624550ab
郭廷杰 <iamnabob@126.com>
2017-11-14 01:32:16 +0800

    add login

---
+++ src/service/User.js
@@ -0,0 +1,74 @@
+'use strict'
+/**
+ * Service
+ */
+import validator from 'validator';
+
+module.exports = function(done) {
+    $.method('user.add').check({
+        name: {require: true, validate: (v) => validator.isLength(v, {min: 4, max: 20}) && /^[A-Za-z]/.test(v)},
+        emai: {require: true, validate: (v) => validator.isEmail(v)},
+        password: {require: true, validate: (v) => validator.isLength(v, {min: 6})}
+    });
+
+    $.method('user.add').register(async function(params, callback) {
+        params.name = params.name.toLowerCase();
+        // 块及作用域
+        {
+            const user = await $.method('user.get').call({name: params.name})
+            if(user) return callback(new Error(`User ${params.name} is already exit.`));
+        }
+
+        {
+            const user = await $.method('user.get').call({email: params.email})
+            if(user) return callback(new Error(`User ${params.email} is already exit.`));
+        }
+
+        params.password = $.utils.encryptPassword(params.password.toString());
+        const user = new $.model.User(params);
+
+        user.save(callback);
+    });
+
+    $.method('user.get').check({
+        _id: {validate: (v) => validator.isMongoId(v)},
+        name: {require: true, validate: (v) => validator.isLength(v, {min: 4, max: 20}) && /^[A-Za-z]/.test(v)},
+        emai: {require: true, validate: (v) => validator.isEmail(v)}
+    });
+    $.method('user.get').register(async function(params, callback) {
+        const query = {};
+        if(params._id) {
+            query._id = params._id;
+        }else if(params.name) {
+            query.name = params.name;
+        } else if(params.email) {
+            query.email = params.email;
+        } else {
+            return callback(new Error('missing parametor _id | name | email'));
+        }
+
+        $.model.User.findOne(query, callback);
+    });
+
+    $.method('user.update').check({
+        _id: {validate: (v) => validator.isMongoId(v)},
+        name: {require: true, validate: (v) => validator.isLength(v, {min: 4, max: 20}) && /^[A-Za-z]/.test(v)},
+        emai: {require: true, validate: (v) => validator.isEmail(v)}
+    });
+
+    $.method('user.update').register(async function(params, callback) {
+        const user = await $.method('user.get').call(params);
+        if(!user) return callback(new Error('user dones\'t exit.'));
+
+        const update = {};
+        if(params.name) update.name = params.name;
+        if(params.email) update.email = params.email;
+        if(params.password) update.password = params.password;
+        if(params.nickname) update.nickname = params.nickname;
+        if(params.about) update.about = params.about;
+        
+        $.model.User.update({_id: user._id}, {$set: update}, callback);
+    });
+
+    done();
+}
